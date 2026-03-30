953fdd695324b2a804641a9545794bc9481a4967
Kauã Victor <164793271+kauavictorss@users.noreply.github.com>
2026-03-29 22:31:52 -0300

    Modificando categorias

--- catalogo/js/data.js
+++ catalogo/js/data.js
@@ -1,13 +1,13 @@
 export const categories = [
     {
-        title: "Épicos",
+        title: "Assistindo",
         items: [
             {
-                img: "https://ingresso-a.akamaihd.net/b2b/production/uploads/article/image/4722/4c88559deb7f7d7354a71d920447113f.jpg", top10: true, badge: "Clássico", badgeColor: "red", progress: 0,
+                img: "https://ingresso-a.akamaihd.net/b2b/production/uploads/article/image/4722/4c88559deb7f7d7354a71d920447113f.jpg", top10: true, badge: "Clássico", badgeColor: "red", progress: 1,
                 youtube: "https://www.youtube.com/watch?v=3g6WChxY14I"
             },
             {
-                img: "https://disney.images.edge.bamgrid.com/ripcut-delivery/v2/variant/disney/34cc591f-ae1b-44ed-bf8a-c4bbef84d588/compose?aspectRatio=1.78&format=webp&width=1200", progress: 90,
+                img: "https://disney.images.edge.bamgrid.com/ripcut-delivery/v2/variant/disney/34cc591f-ae1b-44ed-bf8a-c4bbef84d588/compose?aspectRatio=1.78&format=webp&width=1200", progress: 95,
                 youtube: "https://www.youtube.com/watch?v=lFzVJEksoDY"
             },
             {
@@ -23,19 +23,19 @@ export const categories = [
     {
         title: "Séries",
         items: [
-            { img: "https://classic.exame.com/wp-content/uploads/2022/08/Todo-Mundo-Odeia-o-Chris.jpg", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://segredosdomundo.r7.com/wp-content/uploads/2015/05/veja-como-esta-o-elenco-da-serie-eu-a-patroa-e-as-criancas.jpg", top10: true, youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://www.cafecomblablabla.com.br/wp-content/uploads/2021/09/data-de-estreia-u%CC%81ltima-temporada-brooklyn-99-.jpg", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://m.media-amazon.com/images/S/pv-target-images/bee17fc2ca06b32b3e685ebbbc9e0affd43a105eca5d88680ffde032c4f76280.jpg", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://classic.exame.com/wp-content/uploads/2022/08/Todo-Mundo-Odeia-o-Chris.jpg", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=HBV6fTT_SH8" },
+            { img: "https://m.media-amazon.com/images/S/pv-target-images/beea527c66ed3d5b147f6833a8320e52b2279b1acbbc59959d6dc2c3d4030f37.jpg", top10: true, youtube: "https://www.youtube.com/watch?v=sxufB6DxXk0" },
+            { img: "https://www.cafecomblablabla.com.br/wp-content/uploads/2021/09/data-de-estreia-u%CC%81ltima-temporada-brooklyn-99-.jpg", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=sEOuJ4z5aTc" },
+            { img: "https://m.media-amazon.com/images/S/pv-target-images/bee17fc2ca06b32b3e685ebbbc9e0affd43a105eca5d88680ffde032c4f76280.jpg", badge: "Novidade", badgeColor: "green", youtube: "https://www.youtube.com/watch?v=VoIn9rH9zHY" },
         ]
     },
     {
         title: "Para maratonar",
         items: [
-            { img: "https://ingresso-a.akamaihd.net/b2b/production/uploads/article/image/4866/57bc10c2da6444373eb49152a4d140e2.jpg", top10: true, youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://segredosdomundo.r7.com/wp-content/uploads/2020/08/friends-curiosidades-sobre-uma-das-maiores-series-de-todos-os-tempos.jpg", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://m.media-amazon.com/images/S/pv-target-images/15c3535d4eb49f808d8d105d95a0dfddc3ec60604086e0f75f23145e6ce7c257.jpg", top10: true, badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
-            { img: "https://disney.images.edge.bamgrid.com/ripcut-delivery/v2/variant/disney/7ac3d441-bb63-46fb-b52d-3a98d5e29c46/compose?aspectRatio=1.78&format=webp&width=1200", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://ingresso-a.akamaihd.net/b2b/production/uploads/article/image/4866/57bc10c2da6444373eb49152a4d140e2.jpg", top10: true, youtube: "https://www.youtube.com/watch?v=3UcF6Jq3sAY" },
+            { img: "https://static.wikia.nocookie.net/comotreinarseudragao/images/7/7d/Drag%C3%B5es_Corrida_At%C3%A9_O_Limite.jpg/revision/latest?cb=20150920085550&path-prefix=pt-br", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=NWBmTdsalws" },
+            { img: "https://m.media-amazon.com/images/S/pv-target-images/15c3535d4eb49f808d8d105d95a0dfddc3ec60604086e0f75f23145e6ce7c257.jpg", top10: true, badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=0fWjd__JJhg" },
+            { img: "https://disney.images.edge.bamgrid.com/ripcut-delivery/v2/variant/disney/7ac3d441-bb63-46fb-b52d-3a98d5e29c46/compose?aspectRatio=1.78&format=webp&width=1200", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=CLPoxiIR2ss" },
         ]
     }
 ];
