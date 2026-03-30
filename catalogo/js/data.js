10803fa072f269615c91c827a66c943d48b35b10
Kauã Victor <164793271+kauavictorss@users.noreply.github.com>
2026-03-29 22:33:38 -0300

    Indentando scripts

--- catalogo/js/data.js
+++ catalogo/js/data.js
@@ -3,19 +3,26 @@ export const categories = [
         title: "Assistindo",
         items: [
             {
-                img: "https://ingresso-a.akamaihd.net/b2b/production/uploads/article/image/4722/4c88559deb7f7d7354a71d920447113f.jpg", top10: true, badge: "Clássico", badgeColor: "red", progress: 1,
+                img: "https://ingresso-a.akamaihd.net/b2b/production/uploads/article/image/4722/4c88559deb7f7d7354a71d920447113f.jpg",
+                top10: true,
+                badge: "Clássico",
+                badgeColor: "red",
+                progress: 1,
                 youtube: "https://www.youtube.com/watch?v=3g6WChxY14I"
             },
             {
-                img: "https://disney.images.edge.bamgrid.com/ripcut-delivery/v2/variant/disney/34cc591f-ae1b-44ed-bf8a-c4bbef84d588/compose?aspectRatio=1.78&format=webp&width=1200", progress: 95,
+                img: "https://disney.images.edge.bamgrid.com/ripcut-delivery/v2/variant/disney/34cc591f-ae1b-44ed-bf8a-c4bbef84d588/compose?aspectRatio=1.78&format=webp&width=1200",
+                progress: 95,
                 youtube: "https://www.youtube.com/watch?v=lFzVJEksoDY"
             },
             {
-                img: "https://m.media-amazon.com/images/S/pv-target-images/b71b06873fb90830776f41793940dddf06e5f38de22aa19e456d1aa3e1da483f.jpg", progress: 75,
+                img: "https://m.media-amazon.com/images/S/pv-target-images/b71b06873fb90830776f41793940dddf06e5f38de22aa19e456d1aa3e1da483f.jpg",
+                progress: 75,
                 youtube: "https://www.youtube.com/watch?v=HKVve_VSz58"
             },
             {
-                img: "https://preview.redd.it/who-do-you-think-is-the-best-written-kung-fu-panda-character-v0-jy0a4llnjk4e1.jpeg?width=640&crop=smart&auto=webp&s=b4c5bd71710b2905169b8cb043f7930b0e79ee97", progress: 10,
+                img: "https://preview.redd.it/who-do-you-think-is-the-best-written-kung-fu-panda-character-v0-jy0a4llnjk4e1.jpeg?width=640&crop=smart&auto=webp&s=b4c5bd71710b2905169b8cb043f7930b0e79ee97",
+                progress: 10,
                 youtube: "https://www.youtube.com/watch?v=40IBUnOZoC4"
             },
         ]
@@ -23,19 +30,61 @@ export const categories = [
     {
         title: "Séries",
         items: [
-            { img: "https://classic.exame.com/wp-content/uploads/2022/08/Todo-Mundo-Odeia-o-Chris.jpg", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=HBV6fTT_SH8" },
-            { img: "https://m.media-amazon.com/images/S/pv-target-images/beea527c66ed3d5b147f6833a8320e52b2279b1acbbc59959d6dc2c3d4030f37.jpg", top10: true, youtube: "https://www.youtube.com/watch?v=sxufB6DxXk0" },
-            { img: "https://www.cafecomblablabla.com.br/wp-content/uploads/2021/09/data-de-estreia-u%CC%81ltima-temporada-brooklyn-99-.jpg", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=sEOuJ4z5aTc" },
-            { img: "https://m.media-amazon.com/images/S/pv-target-images/bee17fc2ca06b32b3e685ebbbc9e0affd43a105eca5d88680ffde032c4f76280.jpg", badge: "Novidade", badgeColor: "green", youtube: "https://www.youtube.com/watch?v=VoIn9rH9zHY" },
+            {
+                img: "https://classic.exame.com/wp-content/uploads/2022/08/Todo-Mundo-Odeia-o-Chris.jpg",
+                top10: true,
+                badge: "Nova temporada",
+                badgeColor: "red",
+                youtube: "https://www.youtube.com/watch?v=HBV6fTT_SH8"
+            },
+            {
+                img: "https://m.media-amazon.com/images/S/pv-target-images/beea527c66ed3d5b147f6833a8320e52b2279b1acbbc59959d6dc2c3d4030f37.jpg",
+                top10: true,
+                youtube: "https://www.youtube.com/watch?v=sxufB6DxXk0"
+            },
+            {
+                img: "https://www.cafecomblablabla.com.br/wp-content/uploads/2021/09/data-de-estreia-u%CC%81ltima-temporada-brooklyn-99-.jpg",
+                badge: "Novo episódio",
+                badgeColor: "red",
+                youtube: "https://www.youtube.com/watch?v=sEOuJ4z5aTc"
+            },
+            {
+                img: "https://m.media-amazon.com/images/S/pv-target-images/bee17fc2ca06b32b3e685ebbbc9e0affd43a105eca5d88680ffde032c4f76280.jpg",
+                badge: "Novidade",
+                badgeColor: "green",
+                youtube: "https://www.youtube.com/watch?v=VoIn9rH9zHY"
+            },
         ]
     },
     {
         title: "Para maratonar",
         items: [
-            { img: "https://ingresso-a.akamaihd.net/b2b/production/uploads/article/image/4866/57bc10c2da6444373eb49152a4d140e2.jpg", top10: true, youtube: "https://www.youtube.com/watch?v=3UcF6Jq3sAY" },
-            { img: "https://static.wikia.nocookie.net/comotreinarseudragao/images/7/7d/Drag%C3%B5es_Corrida_At%C3%A9_O_Limite.jpg/revision/latest?cb=20150920085550&path-prefix=pt-br", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=NWBmTdsalws" },
-            { img: "https://m.media-amazon.com/images/S/pv-target-images/15c3535d4eb49f808d8d105d95a0dfddc3ec60604086e0f75f23145e6ce7c257.jpg", top10: true, badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=0fWjd__JJhg" },
-            { img: "https://disney.images.edge.bamgrid.com/ripcut-delivery/v2/variant/disney/7ac3d441-bb63-46fb-b52d-3a98d5e29c46/compose?aspectRatio=1.78&format=webp&width=1200", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=CLPoxiIR2ss" },
+            {
+                img: "https://ingresso-a.akamaihd.net/b2b/production/uploads/article/image/4866/57bc10c2da6444373eb49152a4d140e2.jpg",
+                top10: true,
+                youtube: "https://www.youtube.com/watch?v=3UcF6Jq3sAY"
+            },
+            {
+                img: "https://static.wikia.nocookie.net/comotreinarseudragao/images/7/7d/Drag%C3%B5es_Corrida_At%C3%A9_O_Limite.jpg/revision/latest?cb=20150920085550&path-prefix=pt-br",
+                top10: true,
+                badge: "Novo episódio",
+                badgeColor: "red",
+                youtube: "https://www.youtube.com/watch?v=NWBmTdsalws"
+            },
+            {
+                img: "https://m.media-amazon.com/images/S/pv-target-images/15c3535d4eb49f808d8d105d95a0dfddc3ec60604086e0f75f23145e6ce7c257.jpg",
+                top10: true,
+                badge: "Novidade",
+                badgeColor: "red",
+                youtube: "https://www.youtube.com/watch?v=0fWjd__JJhg"
+            },
+            {
+                img: "https://disney.images.edge.bamgrid.com/ripcut-delivery/v2/variant/disney/7ac3d441-bb63-46fb-b52d-3a98d5e29c46/compose?aspectRatio=1.78&format=webp&width=1200",
+                top10: true,
+                badge: "Novo episódio",
+                badgeColor: "red",
+                youtube: "https://www.youtube.com/watch?v=CLPoxiIR2ss"
+            },
         ]
     }
 ];
